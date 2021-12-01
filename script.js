4ab50f9eac9de0501d35bcc59538145022050995
Yatsiuk Oleksandr <saj87@bk.ru>
2021-12-01 16:05:32 +0200

    add script to tabmenu

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
-'use strict'
-
-document.addEventListener('DOMContentLoaded', () =>{
-
-
-    const hamburgerMenu = document.querySelector('.mob-menu'),
-    desktopMenu = document.querySelector('.wrapper-menu');
-
-
-hamburgerMenu.addEventListener('click',(event)=>{
-  event.preventDefault();
- 
-  desktopMenu.classList.toggle('hamburger-menu-style');
-})
-
-})
+window.addEventListener('DOMContentLoaded', function () {
 
 
 
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items'),
+        mobMenu = document.querySelector('.mob-menu');
+
+    mobMenu.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        tabsParent.classList.toggle('hamburger-menu-style');
+    });
+
+    function hideTabsContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show');
+        });
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    };
+
+    function showTabsContent(i = 0) {
+        tabsContent[i].classList.add('show');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabsContent();
+    showTabsContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabsContent();
+                    showTabsContent(i);
+                }
+            })
+        }
+    })
+
+
+
+
+});
